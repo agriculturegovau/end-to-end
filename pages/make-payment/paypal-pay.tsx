67be16a1d0dff99dbcc37a70d362f9751e7dbb61
Peter DeGroff <212986+dagoof@users.noreply.github.com>
2022-01-13 15:58:19 +1100

    chore: clean up imports, use Todo component

--- pages/make-payment/paypal-pay.tsx
+++ pages/make-payment/paypal-pay.tsx
@@ -2,20 +2,17 @@ import AUbutton from 'components/@gov.au/AUbutton';
 import { WizardLayout } from 'components/layouts/WizardLayout';
 import Link from 'next/link';
 import type { NextPage } from 'next';
-import AUheading from 'components/@gov.au/AUheading';
-import AUcallout from 'components/@gov.au/AUcallout';
-import { ButtonGroup } from 'components/button-group';
-import AUformGroup from 'components/@gov.au/AUformGroup';
-import AUcheckbox from 'components/@gov.au/AUcheckbox';
-import AUfieldset from 'components/@gov.au/AUfieldset';
-import AUlegend from 'components/@gov.au/AUlegend';
+import Todo from 'components/Todo';
 
 const Page: NextPage = () => (
   <WizardLayout title="Paypal" navigator={false} backbreadcrumb={false}>
-    TODO
-    <Link passHref href="/make-payment/paid">
-      <AUbutton link>Pay</AUbutton>
-    </Link>
+    <Todo>
+      <div>
+        <Link passHref href="/make-payment/paid">
+          <AUbutton link>Pay</AUbutton>
+        </Link>
+      </div>
+    </Todo>
   </WizardLayout>
 );
 
--- pages/make-payment/paypal.tsx
+++ pages/make-payment/paypal.tsx
@@ -9,13 +9,17 @@ import AUformGroup from 'components/@gov.au/AUformGroup';
 import AUcheckbox from 'components/@gov.au/AUcheckbox';
 import AUfieldset from 'components/@gov.au/AUfieldset';
 import AUlegend from 'components/@gov.au/AUlegend';
+import Todo from 'components/Todo';
 
 const Page: NextPage = () => (
   <WizardLayout title="Paypal" navigator={false} backbreadcrumb={false}>
-    TODO
-    <Link passHref href="/make-payment/paypal-pay">
-      <AUbutton link>Pay</AUbutton>
-    </Link>
+    <Todo>
+      <div>
+        <Link passHref href="/make-payment/paypal-pay">
+          <AUbutton link>Pay</AUbutton>
+        </Link>
+      </div>
+    </Todo>
   </WizardLayout>
 );
 

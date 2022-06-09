0b13d35fbd5aefeda641646ecab7949ad30fedd4
Peter DeGroff <212986+dagoof@users.noreply.github.com>
2022-06-09 12:01:08 +1000

    feat(find-establishment): add more example establishments

--- components/exampleEstablishments.tsx
+++ components/exampleEstablishments.tsx
@@ -54,4 +54,68 @@ export const establishments: Establishment[] = [
       { action: 'Storing', subject: 'dairy commodity(unrefrigerated)' },
     ],
   },
+  {
+    estNo: '17',
+    name: 'DOCKSIDE COLDSTORE PTY LTD',
+    postCode: '3012',
+    state: 'VIC',
+    operations: [
+      { action: 'Loading', subject: 'dairy commodity containers' },
+      { action: 'Storing', subject: 'dairy commodity(chilled, frozen)' },
+      { action: 'Validating', subject: 'export certification' },
+    ],
+  },
+  {
+    estNo: '44',
+    name: 'AUSTRACOLD (VIC) PTY LTD',
+    postCode: '3062',
+    state: 'VIC',
+    operations: [
+      { action: 'Loading', subject: 'dairy commodity containers' },
+      { action: 'Storing', subject: 'dairy commodity(chilled, frozen, unrefrigerated)' },
+      { action: 'Validating', subject: 'export certification' },
+    ],
+  },
+
+  {
+    estNo: '45',
+    name: 'LAVERTON COLD STORAGE PTY LTD',
+    postCode: '3029',
+    state: 'VIC',
+    operations: [
+      { action: 'Storing', subject: 'dairy commodity(chilled, frozen)' },
+      { action: 'Validating', subject: 'export certification' },
+    ],
+  },
+  {
+    estNo: '56',
+    name: 'DARNUM PARK PTY LTD',
+    postCode: '3149',
+    state: 'VIC',
+    operations: [
+      { action: 'Producing', subject: 'condensed milk and condensed milk prods' },
+      { action: 'Producing', subject: 'cream and cream products' },
+      { action: 'Producing', subject: 'dried milk and dried milk products' },
+      { action: 'Producing', subject: 'infant powder' },
+      { action: 'Producing', subject: 'stockfood (powder)' },
+      { action: 'Producing', subject: 'whey & whey products' },
+      { action: 'Packing', subject: 'condensed milk and condensed milk prods' },
+      { action: 'Packing', subject: 'cream and cream products' },
+      { action: 'Packing', subject: 'dried milk and dried milk products' },
+      { action: 'Packing', subject: 'infant powder' },
+      { action: 'Storing', subject: 'dairy commodity(unrefrigerated)' },
+    ],
+  },
+  {
+    estNo: '57',
+    name: 'H. J. HEINZ COMPANY AUSTRALIA LIMITED',
+    postCode: '3006',
+    state: 'VIC',
+    operations: [
+      { action: 'Producing', subject: 'dairy desserts' },
+      { action: 'Producing', subject: 'yogurt' },
+      { action: 'Storing', subject: 'dairy commodity(chilled, frozen, unrefrigerated)' },
+      { action: 'Validating', subject: 'export certification' },
+    ],
+  },
 ];
